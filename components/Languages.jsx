6b3af17a77932830ef3f812893ab60dc4c1790b2
Mintesnot722 <mintesinotbamlaku@gmail.com>
2026-07-01 18:20:15 +0300

    adding it's functionalities and styles

--- components/Languages.jsx
+++ components/Languages.jsx
@@ -1,12 +1,8 @@
 const Languages = (props) => {
   return (
-    <div className="text-black ">
-      <p
-        className={`text-nowrap w-auto rounded text-center h-6.25 gap-[4.5px]  ${props.color}`}
-      >
-        {props.lang}
-      </p>
-    </div>
+    <span className={`rounded-[3px]  p-[4.5px]  ${props.color}`}>
+      {props.lang}
+    </span>
   );
 };
 
